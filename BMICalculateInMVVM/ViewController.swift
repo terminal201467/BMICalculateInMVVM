b59b0a901559dcac0423327c0f9a465e7e23985b
MJ <terminal201467@gmail.com>
2022-04-20 16:03:46 +0800

    Initial Commit

---
+++ BMICalculateInMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMICalculateInMVVM
+//
+//  Created by Jhen Mu on 2022/4/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
